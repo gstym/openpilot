fdc37d159a7e92c062bb3d4f9137a0f95d346b53
gstym <96527360+gstym@users.noreply.github.com>
2024-04-09 07:20:07 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/legacy_longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/legacy_longitudinal_mpc_lib/long_mpc.py
@@ -52,7 +52,7 @@ T_IDXS = np.array(T_IDXS_LST)
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 MIN_ACCEL = -3.5
 COMFORT_BRAKE = 2.5
-STOP_DISTANCE = 6.0
+STOP_DISTANCE = 4.5
 
 def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
