dd3ab17031f8b6783bbb8f6124f9201a34ea5d0a
gstym <96527360+gstym@users.noreply.github.com>
2025-09-30 12:51:23 +0800

    Update vibe_personality.py

--- sunnypilot/selfdrive/controls/lib/vibe_personality/vibe_personality.py
+++ sunnypilot/selfdrive/controls/lib/vibe_personality/vibe_personality.py
@@ -15,7 +15,7 @@ AccelPersonality = custom.LongitudinalPlanSP.AccelerationPersonality
 
 # Acceleration Profiles mapped to AccelPersonality (eco/normal/sport)
 MAX_ACCEL_PROFILES = {
-    AccelPersonality.eco:       [1.7,  1.30,  1.10, 0.70, .500, .292, .13, .09],  # eco
+    AccelPersonality.eco:       [1.7,  1.60,  1.20, 0.80, .500, .292, .13, .09],  # eco
     AccelPersonality.normal:    [2.0,  1.95,  1.85, 1.30, .650, .310, .17, .12],   # normal
     AccelPersonality.sport:     [2.0,  2.00,  1.99, 1.65, .800, .333, .24, .2],    # sport
 }
@@ -23,9 +23,9 @@ MAX_ACCEL_BREAKPOINTS =         [0.,   4.,   6.,   9.,   16.,  25.,  30., 55.]
 
 # Braking profiles mapped to LongPersonality (relaxed/standard/aggressive)
 MIN_ACCEL_PROFILES = {
-    LongPersonality.relaxed:    [-.2, -.5, -.8,  -1.15, -1.4],  # gentler braking
-    LongPersonality.standard:   [-.2, -.5, -.8,  -1.15, -1.4],  # normal braking
-    LongPersonality.aggressive: [-.2, -.5, -.8,  -1.15, -1.4],  # more aggressive braking
+    LongPersonality.relaxed:    [-0.6, -0.8, -1.0,  -1.2, -1.4],  # gentler braking
+    LongPersonality.standard:   [-0.6, -0.8, -1.0,  -1.2, -1.4],  # normal braking
+    LongPersonality.aggressive: [-0.6, -0.8, -1.0,  -1.2, -1.4],  # more aggressive braking
 }
 MIN_ACCEL_BREAKPOINTS =         [0.,   1.5,    4.,   9.,  19.]
 
