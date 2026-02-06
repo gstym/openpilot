687d488bc135206b563469c48ec005ab791e3f28
gstym <96527360+gstym@users.noreply.github.com>
2026-02-06 13:13:34 +0800

    Update vision_turn_controller.py

--- sunnypilot/selfdrive/controls/lib/vision_turn_controller.py
+++ sunnypilot/selfdrive/controls/lib/vision_turn_controller.py
@@ -17,10 +17,10 @@ _MIN_V = 20 * CV.KPH_TO_MS  # Do not operate under 20 km/h
 _ENTERING_PRED_LAT_ACC_TH = 0.9  # Predicted Lat Acc threshold to trigger entering turn state.
 _ABORT_ENTERING_PRED_LAT_ACC_TH = 0.6  # Predicted Lat Acc threshold to abort entering state if speed drops.
 
-_TURNING_LAT_ACC_TH = 1.6  # Lat Acc threshold to trigger turning state.
+_TURNING_LAT_ACC_TH = 1.1  # Lat Acc threshold to trigger turning state.
 
-_LEAVING_LAT_ACC_TH = 1.3  # Lat Acc threshold to trigger leaving turn state.
-_FINISH_LAT_ACC_TH = 1.1  # Lat Acc threshold to trigger the end of the turn cycle.
+_LEAVING_LAT_ACC_TH = 0.9  # Lat Acc threshold to trigger leaving turn state.
+_FINISH_LAT_ACC_TH = 0.7  # Lat Acc threshold to trigger the end of the turn cycle.
 
 _EVAL_STEP = 5.  # mts. Resolution of the curvature evaluation.
 _EVAL_START = 20.  # mts. Distance ahead where to start evaluating vision curvature.
@@ -39,7 +39,7 @@ _ENTERING_SMOOTH_DECEL_BP = [1.3, 3.]  # absolute value of lat acc ahead
 # Lookup table for the acceleration for the TURNING state
 # depending on the current lateral acceleration of the vehicle.
 _TURNING_ACC_V = [0.2, 0., -0.6]  # acc value
-_TURNING_ACC_BP = [1.5, 2.0, 3.]  # absolute value of current lat acc
+_TURNING_ACC_BP = [1.0, 1.8, 3.]  # absolute value of current lat acc
 
 _LEAVING_ACC = 0.5  # Conformable acceleration to regain speed while leaving a turn.
 
