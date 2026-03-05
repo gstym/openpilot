df57e867f2cc1cd707d838508fd2efe742304dcf
gstym <96527360+gstym@users.noreply.github.com>
2026-03-05 19:13:58 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -66,7 +66,7 @@ def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
   elif personality==log.LongitudinalPersonality.standard:
     return 1.0
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 0.5
+    return 0.75
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
