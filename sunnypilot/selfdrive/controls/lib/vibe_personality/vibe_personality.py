44d021d6125c0f28331b7ed65e16cd1b762bcb23
gstym <96527360+gstym@users.noreply.github.com>
2026-04-08 09:40:44 +0800

    Update vibe_personality.py

--- sunnypilot/selfdrive/controls/lib/vibe_personality/vibe_personality.py
+++ sunnypilot/selfdrive/controls/lib/vibe_personality/vibe_personality.py
@@ -42,7 +42,7 @@ def get_T_FOLLOW_vibe(personality):
 
 def get_dynamic_personality(v_ego, personality):
     """Adjust T_FOLLOW based on vehicle speed"""
-    scale_factor = np.interp(v_ego, [0, 18, 28], [0.62, 0.74, 1.0])
+    scale_factor = np.interp(v_ego, [0, 18, 28], [0.60, 0.7, 1.0])
     return get_T_FOLLOW_vibe(personality) * scale_factor
 
 
