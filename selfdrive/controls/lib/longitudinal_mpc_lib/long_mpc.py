f95d97ce6c58f2919398c342e505fd07011c717c
gstym <96527360+gstym@users.noreply.github.com>
2026-05-06 10:42:38 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -412,7 +412,8 @@ class LongitudinalMpc:
       self.solver.set(i, "yref", self.yref[i])
     self.solver.set(N, "yref", self.yref[N][:COST_E_DIM])
 
-    self.params[:,2] = np.min(x_obstacles, axis=1)
+    STOP_DISTANCE_ADJUST = 1.5
+    self.params[:,2] = np.min(x_obstacles, axis=1) + STOP_DISTANCE_ADJUST
     self.params[:,3] = np.copy(self.prev_a)
     self.params[:,4] = t_follow
 
